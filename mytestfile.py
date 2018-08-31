6c3abfcf48c04fb176476d9d134a6efc5f567107
Muhammad Shoaib <muhammad.shoaib@toyotaconnected.com>
2018-08-31 16:01:38 -0500

    test update

--- mytestfile.py
+++ mytestfile.py
@@ -8,7 +8,11 @@ class TestUM(unittest.TestCase):
  
     def test_numbers_3_4(self):
         self.assertEqual( 3*4, 12)
- 
+
+    def test_numbers_5_4(self):
+        self.assertEqual( 5*4, 20) 
+
+
     def test_strings_a_3(self):
         self.assertEqual('a'*3, 'aaa')
  
